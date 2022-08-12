cd30b444d3442a946094066e3bc2b1a2b9b7cf6a
ShadowHunter15 <72161983+ShadowHunter15@users.noreply.github.com>
2022-08-12 23:28:38 +0200

    added ping-meter to 'added ping-meter to measure the ping and give a general idea of the internet speed'

--- onlineservices.py
+++ onlineservices.py
@@ -1,2 +1,7 @@
+import subprocess
+from subprocess import PIPE
 class SmartWallpaperOnlineServies():
-    pass
+    def getInternetPing(self):
+        hostname = "www.google.com"
+        response = subprocess.run("ping -n 1 " + hostname, stdout=PIPE, stderr=PIPE)
+        return response
